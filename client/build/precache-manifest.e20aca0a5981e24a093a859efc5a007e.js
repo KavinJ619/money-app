6c6821b34987f2b0b49ef4ae839753fbb48b7524
BuildTools <kavinj619@gmail.com>
2020-04-27 21:27:15 +0900

    8th attempt

---
+++ client/build/precache-manifest.e20aca0a5981e24a093a859efc5a007e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390ccbd697c475f82cd64c62c8cd397b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b70890ac73422210b0",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8d08b818f3b7776ddbf8",
+    "url": "/static/css/main.ed1ad220.chunk.css"
+  },
+  {
+    "revision": "c7b70890ac73422210b0",
+    "url": "/static/js/2.be89d4cb.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.be89d4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d08b818f3b7776ddbf8",
+    "url": "/static/js/main.e08715a8.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
