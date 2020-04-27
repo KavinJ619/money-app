6d2c1e0f2d382a83d78fe9959cf1341f180d7653
BuildTools <kavinj619@gmail.com>
2020-04-27 22:07:47 +0900

    58403 attemp

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
 
 
 if (process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname, 'client/build/static')));
+    app.use(express.static('client/build'))
 
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
