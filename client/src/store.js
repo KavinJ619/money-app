43f7e0079f9548dc9544b9d4d04d00a57b73e527
BuildTools <kavinj619@gmail.com>
2020-04-27 18:43:24 +0900

    removed devtools from store

--- client/src/store.js
+++ client/src/store.js
@@ -2,22 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers"
 
-const initialState = {};
+const initialState={};
 
+const middleware= [thunk];
 
-const middleware = [thunk];
-
-
-const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-
-
-const enhancer = composeEnhancers(applyMiddleware(...middleware));
-
-
-const store = createStore(
-rootReducer,
-initialState,
-enhancer
-);
+const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware)
+));
 
 export default store;
