f43cfec604fa74d1ab4fc93da5f096e7c7b7c0ac
BuildTools <kavinj619@gmail.com>
2020-04-27 21:36:04 +0900

    100th attempt

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
 
 
 if (process.env.NODE_ENV==='production'){
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build/static')));
 
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
