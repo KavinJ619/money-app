eb45229de4bd68805809d89ed39bd2a75335b873
BuildTools <kavinj619@gmail.com>
2020-04-27 21:15:19 +0900

    6th attempt

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ mongoose
 
 
 if (process.env.NODE_ENV==='production'){
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    const root = require('path').join(__dirname, 'client', 'build')
+    app.use(express.static(root));
+    app.get("*", (req, res) => {
+        res.sendFile('index.html', { root });
+    })
 }
 
 
